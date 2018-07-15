4353a71665b76f39fc4898e0c5f2e90c766ee116
s3rl3g <serleg.dani@live.com>
2018-07-15 16:49:02 +0200

    location detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,14 @@ import { Component, HostListener, ViewChild, AfterViewInit, ElementRef } from '@
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements AfterViewInit {
+  @ViewChild('openingLink') openingLinkRef: ElementRef;
+  @ViewChild('introLink') introLinkRef: ElementRef;
+  @ViewChild('locationLink') locationLinkRef: ElementRef;
+  @ViewChild('roomsLink') roomsLinkRef: ElementRef;
+  @ViewChild('radarLink') radarLinkRef: ElementRef;
+  @ViewChild('bookingLink') bookingLinkRef: ElementRef;
+  @ViewChild('seasonalLink') seasonalLinkRef: ElementRef;
+
   @ViewChild('opening') openingRef: ElementRef;
   @ViewChild('intro') introRef: ElementRef;
   @ViewChild('location') locationRef: ElementRef;
@@ -23,8 +31,6 @@ export class AppComponent implements AfterViewInit {
   ngAfterViewInit() {
     this.winHeight = window.innerHeight;
     this.winWidth = window.innerWidth;
-
-    // ki kell számolni az összes panel helyét (le kell kérdezni)
   }
 
   @HostListener('window:scroll', ['$event'])
@@ -39,13 +45,68 @@ export class AppComponent implements AfterViewInit {
 
     // össze kell hasonlítani az összeset, az van kijelölve amelyiken állunk
 
-    const hT = this.locationRef.nativeElement.offsetTop;
-    const hH = this.locationRef.nativeElement.offsetHeight - 200;
+    const openingTop = this.openingLinkRef.nativeElement.offsetTop;
+    const openingHeight = this.openingLinkRef.nativeElement.offsetHeight - 200;
+
+    const introTop = this.introRef.nativeElement.offsetTop;
+    const introHeight = this.introRef.nativeElement.offsetHeight - 200;
+
+    const locationTop = this.locationRef.nativeElement.offsetTop;
+    const locationHeight = this.locationRef.nativeElement.offsetHeight - 200;
+
+    const roomsTop = this.roomsRef.nativeElement.offsetTop;
+    const roomsHeight = this.roomsRef.nativeElement.offsetHeight - 200;
+
+    const radarTop = this.radarRef.nativeElement.offsetTop;
+    const radarHeight = this.radarRef.nativeElement.offsetHeight - 200;
+
+    const bookingTop = this.bookingRef.nativeElement.offsetTop;
+    const bookingHeight = this.bookingRef.nativeElement.offsetHeight - 200;
+
+    const seasonalTop = this.seasonalRef.nativeElement.offsetTop;
+    const seasonalHeight = this.seasonalRef.nativeElement.offsetHeight - 200;
+
+
     const wH = this.winHeight;
     const wS = currPos;
-    if (wS > (hT + hH - wH)) {
-      console.log('elerte');
+
+    if (wS > (openingTop + openingHeight - wH) &&
+      wS < (introTop + introHeight - wH)) {
+      console.log('opening');
     }
+
+    if (wS > (introTop + introHeight - wH) &&
+      wS < (locationTop + locationHeight - wH)) {
+      console.log('intro');
+    }
+
+    if (wS > (locationTop + locationHeight - wH) &&
+      wS < (roomsTop + roomsHeight - wH)) {
+      console.log('location');
+    }
+
+    if (wS > (roomsTop + roomsHeight - wH) &&
+      wS < (radarTop + radarHeight - wH)) {
+      console.log('rooms');
+    }
+
+    if (wS > (radarTop + radarHeight - wH) &&
+      wS < (bookingTop + bookingHeight - wH)) {
+      console.log('radar');
+    }
+
+    if (wS > (bookingTop + bookingHeight - wH) &&
+      wS < (seasonalTop + seasonalHeight - wH)) {
+      console.log('booking');
+    }
+
+    if (wS > (seasonalTop + seasonalHeight - wH)) {
+      console.log('seasonal');
+    }
+
+    /*if (wS > (hT + hH - wH)) {
+      console.log('elerte');
+    }*/
   }
 
   @HostListener('window:resize', ['$event'])
