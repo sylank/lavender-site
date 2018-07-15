08b322690ae55b62573218b0c56d41aa2576f935
s3rl3g <serleg.dani@live.com>
2018-07-15 14:28:56 +0200

    menu bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'app';
+  isScrolled = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll($event) {
+    const currPos = (window.pageYOffset || $event.target.scrollTop) - ($event.target.clientTop || 0);
+
+    if (currPos > 64) {
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = false;
+    }
+  }
 }
