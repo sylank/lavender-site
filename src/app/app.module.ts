1f3e20e6517e524898a1b462ac87c6d19fd5c85f
s3rl3g <serleg.dani@live.com>
2018-07-15 14:43:55 +0200

    scroll to feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ScrollToModule } from 'ng2-scroll-to-el';
 
 import { AppComponent } from './app.component';
 import { RadarAppModule } from '../../node_modules/event-radar-ng/src/app/app.module';
@@ -11,7 +11,8 @@ import { RadarAppModule } from '../../node_modules/event-radar-ng/src/app/app.mo
   ],
   imports: [
     BrowserModule,
-    RadarAppModule
+    RadarAppModule,
+    ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
