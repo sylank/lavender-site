814ac187bbb696a36a99f9658d1e75ad6d36f97c
s3rl3g <serleg.dani@live.com>
2018-07-15 15:19:43 +0200

    scrolling and activate mennu element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('opening') openingRef: ElementRef;
+  @ViewChild('intro') introRef: ElementRef;
+  @ViewChild('location') locationRef: ElementRef;
+  @ViewChild('rooms') roomsRef: ElementRef;
+  @ViewChild('radar') radarRef: ElementRef;
+  @ViewChild('booking') bookingRef: ElementRef;
+  @ViewChild('seasonal') seasonalRef: ElementRef;
+
+
   isScrolled = false;
+  private winHeight;
+  private winWidth;
+
+  ngAfterViewInit() {
+    this.winHeight = window.innerHeight;
+    this.winWidth = window.innerWidth;
+
+    // ki kell számolni az összes panel helyét (le kell kérdezni)
+  }
 
   @HostListener('window:scroll', ['$event'])
   onScroll($event) {
@@ -17,5 +35,21 @@ export class AppComponent {
     } else {
       this.isScrolled = false;
     }
+
+    // össze kell hasonlítani az összeset, az van kijelölve amelyiken állunk
+
+    const hT = this.locationRef.nativeElement.offsetTop;
+    const hH = this.locationRef.nativeElement.offsetHeight - 200;
+    const wH = this.winHeight;
+    const wS = currPos;
+    if (wS > (hT + hH - wH)) {
+      console.log('elerte');
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.winHeight = event.target.innerHeight;
+    this.winWidth = event.target.innerWidth;
   }
 }
