4479001380991915ce6f374d8c41dfa3a2ed4b2b
serleg.dani <serleg.dani@live.com>
2018-07-24 23:27:48 +0200

    ci cd scripts

---
+++ .circleci/artifactory_copy.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+VERSION=$1 #$(cat package.json | jq '.version' | sed  s/"\""/""/g)
+
+#Delete if exists
+aws s3 rm s3://dev.levendulabalatonmaria.info/static/$VERSION --recursive
+
+#Copy the new content
+aws s3 cp dist s3://dev.levendulabalatonmaria.info/static/$VERSION --recursive
