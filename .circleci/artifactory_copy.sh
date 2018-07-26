549441ee9dd29079c0882c06ad4a5c961e3c33a1
serleg.dani <serleg.dani@live.com>
2018-07-26 11:51:17 +0200

    deploy script update and artifactory copy stript bugfix

--- .circleci/artifactory_copy.sh
+++ .circleci/artifactory_copy.sh
@@ -3,7 +3,7 @@
 VERSION=$1 #$(cat package.json | jq '.version' | sed  s/"\""/""/g)
 
 #Delete if exists
-aws s3 rm s3://dev.levendulabalatonmaria.info/static/$VERSION --recursive
+aws s3 rm s3://artifactory.levendulabalatonmaria.info/static/$VERSION --recursive
 
 #Copy the new content
-aws s3 cp ../dist s3://dev.levendulabalatonmaria.info/static/$VERSION --recursive
+aws s3 cp ../dist s3://artifactory.levendulabalatonmaria.info/static/$VERSION --recursive
