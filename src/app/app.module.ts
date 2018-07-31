c05f4186a390876423245a19fb00bcd01a43fc95
sylank <serleg.dani@live.com>
2018-07-31 18:59:10 +0200

    google maps integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { ScrollToModule } from 'ng2-scroll-to-el';
 import { AppComponent } from './app.component';
 import { RadarAppModule } from '../../node_modules/event-radar-ng/src/app/app.module';
 
+import { AgmCoreModule } from '@agm/core';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,10 @@ import { RadarAppModule } from '../../node_modules/event-radar-ng/src/app/app.mo
   imports: [
     BrowserModule,
     RadarAppModule,
-    ScrollToModule.forRoot()
+    ScrollToModule.forRoot(),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCperAMWUZ78vz9PMdFDJcpmnH8p-AAij0'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
