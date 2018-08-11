4aaf45a26daaa11634a4275dbb07e259ed62a224
Krisztian Kugler <kugler.krisztian@erise.hu>
2018-08-11 14:24:59 +0200

    Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { IntroComponent } from './intro/intro.component';
+import { PhotosComponent } from './photos/photos.component';
+import { EventsComponent } from './events/events.component';
+import { ContactComponent } from './contact/contact.component';
+import { BookingComponent } from './booking/booking.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    IntroComponent,
+    PhotosComponent,
+    EventsComponent,
+    ContactComponent,
+    BookingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
