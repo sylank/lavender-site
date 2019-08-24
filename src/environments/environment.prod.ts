bcd7f54200516ee09d00ff2ca23a0ecc8b4e9c71
Daniel Serleg <serleg.dani@live.com>
2019-08-24 17:03:11 +0200

    Inactivate reservation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'https://zwp7gjz34a.execute-api.eu-central-1.amazonaws.com/api',
-  reservationEnabled: true
+  reservationEnabled: false
 };
