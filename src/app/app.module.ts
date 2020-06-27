6135ada70d4dd4bed28a248d94f8461ff0e4dcaa
Daniel Serleg <serleg.dani@live.com>
2020-06-27 19:36:49 +0200

    Implement house chooser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CookieService } from 'ngx-cookie-service';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CookieService } from "ngx-cookie-service";
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { EventsComponent } from './events/events.component';
-import { ContactComponent } from './contact/contact.component';
-import { BookingComponent } from './booking/booking.component';
-import { AppRoutingModule } from './app-routing.module';
-import { FooterComponent } from './footer/footer.component';
-import { AutofocusDirective } from './shared/autofocus.directive';
-import { GalleryService } from './shared/gallery.service';
-import { HttpClientModule, HttpClientJsonpModule } from '../../node_modules/@angular/common/http';
-import { ImageLoaderDirective } from './shared/image-loader.directive';
-import { ImageOpacityDirective } from './shared/image-opacity.directive';
-import { CalendarComponent } from './booking/calendar/calendar.component';
-import { DayComponent } from './booking/calendar/day/day.component';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { GalleryComponent } from "./gallery/gallery.component";
+import { EventsComponent } from "./events/events.component";
+import { ContactComponent } from "./contact/contact.component";
+import { BookingComponent } from "./booking/booking.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { FooterComponent } from "./footer/footer.component";
+import { AutofocusDirective } from "./shared/autofocus.directive";
+import { GalleryService } from "./shared/gallery.service";
+import {
+  HttpClientModule,
+  HttpClientJsonpModule,
+} from "../../node_modules/@angular/common/http";
+import { ImageLoaderDirective } from "./shared/image-loader.directive";
+import { ImageOpacityDirective } from "./shared/image-opacity.directive";
+import { CalendarComponent } from "./booking/calendar/calendar.component";
+import { DayComponent } from "./booking/calendar/day/day.component";
 
-import { EventComponent } from './events/event/event.component';
-import { LoadingCircleComponent } from './shared/loading-circle/loading-circle.component';
-import { NgxCaptchaModule } from 'ngx-captcha';
-import { DataHiderComponent } from './shared/data-hider/data-hider.component';
-import { DeleteBookingComponent } from './delete-booking/delete-booking.component';
-import { DeleteUserDataComponent } from './delete-user-data/delete-user-data.component';
-import { NumberChooserComponent } from './shared/number-chooser/number-chooser.component';
-import { SubscribeComponent } from './subscribe/subscribe.component';
-import { TranslateDirective } from './shared/translate.directive';
+import { EventComponent } from "./events/event/event.component";
+import { LoadingCircleComponent } from "./shared/loading-circle/loading-circle.component";
+import { NgxCaptchaModule } from "ngx-captcha";
+import { DataHiderComponent } from "./shared/data-hider/data-hider.component";
+import { DeleteBookingComponent } from "./delete-booking/delete-booking.component";
+import { DeleteUserDataComponent } from "./delete-user-data/delete-user-data.component";
+import { NumberChooserComponent } from "./shared/number-chooser/number-chooser.component";
+import { SubscribeComponent } from "./subscribe/subscribe.component";
+import { TranslateDirective } from "./shared/translate.directive";
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CookieBarComponent } from './cookie-bar/cookie-bar.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CookieBarComponent } from "./cookie-bar/cookie-bar.component";
+import { HouseChooserButtonComponent } from "./shared/house-chooser-button/house-chooser-button.component";
 
 @NgModule({
   declarations: [
@@ -56,7 +60,8 @@ import { CookieBarComponent } from './cookie-bar/cookie-bar.component';
     NumberChooserComponent,
     SubscribeComponent,
     TranslateDirective,
-    CookieBarComponent
+    CookieBarComponent,
+    HouseChooserButtonComponent,
   ],
   imports: [
     BrowserModule,
@@ -69,6 +74,6 @@ import { CookieBarComponent } from './cookie-bar/cookie-bar.component';
     BrowserAnimationsModule,
   ],
   providers: [GalleryService, CookieService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,37 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { CalendarService } from './calendar/calendar.service';
+import { Component, OnInit } from "@angular/core";
+import { CalendarService } from "./calendar/calendar.service";
 import {
   NgForm,
   FormBuilder,
   FormGroup,
   FormControl,
-  Validators
-} from '@angular/forms';
-import { CalendarHttpService } from '../shared/calendar.http.service';
-import { Booking } from './booking.model';
-import { CustomValidator } from '../shared/validators/email.validator';
-import { Router, NavigationEnd } from '@angular/router';
-import { ReCaptchaV3Service } from 'ngx-captcha';
-import { BookingData } from './booking.data';
-import { Constants } from '../shared/constants';
-import { CostCalculationHttpService } from '../shared/cost-calculation.http.service';
-import { environment } from '../../environments/environment';
-import { HttpConstants } from '../shared/http.constants';
-import { MailChimpService } from '../shared/mail-chimp.service';
-import { SubscriptionModel } from '../subscribe/subscription-model';
-import { GoogleAnalyticsService } from '../shared/google-analytics.service';
-import { GoogleAnalyticsConstants } from '../shared/google.analytics.constants';
+  Validators,
+} from "@angular/forms";
+import { CalendarHttpService } from "../shared/calendar.http.service";
+import { Booking } from "./booking.model";
+import { CustomValidator } from "../shared/validators/email.validator";
+import { Router, NavigationEnd } from "@angular/router";
+import { ReCaptchaV3Service } from "ngx-captcha";
+import { BookingData } from "./booking.data";
+import { Constants } from "../shared/constants";
+import { CostCalculationHttpService } from "../shared/cost-calculation.http.service";
+import { environment } from "../../environments/environment";
+import { HttpConstants } from "../shared/http.constants";
+import { MailChimpService } from "../shared/mail-chimp.service";
+import { SubscriptionModel } from "../subscribe/subscription-model";
+import { GoogleAnalyticsService } from "../shared/google-analytics.service";
+import { GoogleAnalyticsConstants } from "../shared/google.analytics.constants";
 
 @Component({
-  selector: 'app-booking',
-  templateUrl: './booking.component.pug',
-  styleUrls: ['./booking.component.sass']
+  selector: "app-booking",
+  templateUrl: "./booking.component.pug",
+  styleUrls: ["./booking.component.sass"],
 })
 export class BookingComponent implements OnInit {
   navigationSubscription: any;
 
+  public lavender1Selected: boolean;
+  public lavender2Selected: boolean;
+
   public dataProtection = false;
-  public dataHandling  = false;
+  public dataHandling = false;
   public houseRules = false;
 
   public bookingEnabled = environment.reservationEnabled;
@@ -40,13 +43,13 @@ export class BookingComponent implements OnInit {
   public showLoading = true;
 
   private basePrice = 25000;
-  private calendarMode: 'arrival' | 'departure' = 'arrival';
+  private calendarMode: "arrival" | "departure" = "arrival";
   private showCalendar = false;
   private arrivalCalendarActive = false;
   private departureCalendarActive = false;
   private calendarInitDate: Date; // To determine which date the calendar should open up with
-  public bookingStage: 'person' | 'data' | 'overview' | 'result' = 'person';
-  private bookingResult: 'success' | 'failed' = 'failed';
+  public bookingStage: "person" | "data" | "overview" | "result" = "person";
+  private bookingResult: "success" | "failed" = "failed";
   private messageLength = 300;
   reservedDates = [];
 
@@ -55,12 +58,12 @@ export class BookingComponent implements OnInit {
     departure: new Date(),
     nights: 1,
     price: 25000,
-    fname: '',
-    lname: '',
-    phone: '',
-    email: '',
-    message: '',
-    reservationId: '',
+    fname: "",
+    lname: "",
+    phone: "",
+    email: "",
+    message: "",
+    reservationId: "",
     subscribe: false,
     personCount: 1,
     petCount: 0,
@@ -80,27 +83,39 @@ export class BookingComponent implements OnInit {
     this.navigationSubscription = this.router.events.subscribe((e: any) => {
       // If it is a NavigationEnd event re-initalise the component
       if (e instanceof NavigationEnd) {
-        this.bookingStage = 'person';
-        this.scrollToPosition()
+        this.bookingStage = "person";
+        this.scrollToPosition();
       }
     });
   }
 
+  houseChooserSelected(event) {
+    if (event === "Lavender1") {
+      this.lavender1Selected = true;
+      this.lavender2Selected = false;
+    }
+
+    if (event === "Lavender2") {
+      this.lavender1Selected = false;
+      this.lavender2Selected = true;
+    }
+  }
+
   onMessageInput(): void {
-    this.messageLength = 300 - this.formName.get('message').value.length;
+    this.messageLength = 300 - this.formName.get("message").value.length;
   }
 
   toggleArrivalCalendar(event: PointerEvent): void {
     event.stopPropagation();
     this.departureCalendarActive = false;
-    if (this.showCalendar && this.calendarMode === 'departure') {
-      this.calendarMode = 'arrival';
+    if (this.showCalendar && this.calendarMode === "departure") {
+      this.calendarMode = "arrival";
     } else {
       this.showCalendar = !this.showCalendar;
     }
-    this.calendarMode = 'arrival';
+    this.calendarMode = "arrival";
     this.calendarInitDate = this.booking.arrival;
-    if (this.showCalendar && this.calendarMode === 'arrival') {
+    if (this.showCalendar && this.calendarMode === "arrival") {
       this.arrivalCalendarActive = true;
     } else {
       this.arrivalCalendarActive = false;
@@ -112,21 +127,22 @@ export class BookingComponent implements OnInit {
     this.googleAnalyticsService.eventEmitter(
       GoogleAnalyticsConstants.ARRIVAL_SHOW_EVENT,
       GoogleAnalyticsConstants.OPEN_ACTION,
-      '',
-      1)
+      "",
+      1
+    );
   }
 
   toggleDepartureCalendar(event: PointerEvent): void {
     event.stopPropagation();
     this.arrivalCalendarActive = false;
-    if (this.showCalendar && this.calendarMode === 'arrival') {
-      this.calendarMode = 'departure';
+    if (this.showCalendar && this.calendarMode === "arrival") {
+      this.calendarMode = "departure";
     } else {
       this.showCalendar = !this.showCalendar;
     }
-    this.calendarMode = 'departure';
+    this.calendarMode = "departure";
     this.calendarInitDate = this.booking.departure;
-    if (this.showCalendar && this.calendarMode === 'departure') {
+    if (this.showCalendar && this.calendarMode === "departure") {
       this.departureCalendarActive = true;
     } else {
       this.departureCalendarActive = false;
@@ -138,13 +154,14 @@ export class BookingComponent implements OnInit {
     this.googleAnalyticsService.eventEmitter(
       GoogleAnalyticsConstants.DEPARTURE_SHOW_EVENT,
       GoogleAnalyticsConstants.OPEN_ACTION,
-      '',
-      1)
+      "",
+      1
+    );
   }
 
   setDate(date: Date): void {
     switch (this.calendarMode) {
-      case 'arrival':
+      case "arrival":
         this.booking.arrival = date;
         if (date.getTime() >= this.booking.departure.getTime() - 86400000) {
           this.booking.departure = new Date(
@@ -154,7 +171,7 @@ export class BookingComponent implements OnInit {
           );
         }
         break;
-      case 'departure':
+      case "departure":
         this.booking.departure = date;
         if (date.getTime() < this.booking.arrival.getTime() + 86400000) {
           this.booking.arrival = new Date(
@@ -165,20 +182,23 @@ export class BookingComponent implements OnInit {
         }
     }
     this.setPrice();
-    this.calendarHttpService.getReservedDates(this.booking.arrival, this.booking.departure).subscribe(
-      (reservedDates: any) => {
+    this.calendarHttpService
+      .getReservedDates(this.booking.arrival, this.booking.departure)
+      .subscribe((reservedDates: any) => {
         this.reservedDates = reservedDates.response.reservations;
-      }
-    );
+      });
 
     this.showLoading = true;
-    this.costCalculationHttpService.getCostCalculationBetweenDate(this.booking.arrival, this.booking.departure).subscribe(
-      (costData: any) => {
+    this.costCalculationHttpService
+      .getCostCalculationBetweenDate(
+        this.booking.arrival,
+        this.booking.departure
+      )
+      .subscribe((costData: any) => {
         this.basePrice = costData.response.value;
         this.setPrice();
         this.showLoading = false;
-      }
-    );
+      });
   }
 
   setPrice(): void {
@@ -204,124 +224,146 @@ export class BookingComponent implements OnInit {
   }
 
   onSubmit(form: NgForm): void {
-    this.bookingStage = 'overview';
-    this.dataProtection = false
-    this.dataHandling = false
-    this.houseRules = false
-    this.booking.subscribe = false
+    this.bookingStage = "overview";
+    this.dataProtection = false;
+    this.dataHandling = false;
+    this.houseRules = false;
+    this.booking.subscribe = false;
 
-    this.scrollToPosition()
+    this.scrollToPosition();
     this.googleAnalyticsService.eventEmitter(
       GoogleAnalyticsConstants.PERSONAL_DATA_SUBMIT_EVENT,
       GoogleAnalyticsConstants.TRUE_ACTION,
-      '',
-      1)
+      "",
+      1
+    );
   }
 
   onPersonSubmit(): void {
-    this.bookingStage = 'data';
+    this.bookingStage = "data";
 
-    this.scrollToPosition()
+    this.scrollToPosition();
 
     this.googleAnalyticsService.eventEmitter(
       GoogleAnalyticsConstants.PEOPLE_DATA_SUBMIT_EVENT,
       GoogleAnalyticsConstants.TRUE_ACTION,
-      '',
-      1)
+      "",
+      1
+    );
   }
 
   isSendingDisabled() {
-    return !(this.dataProtection && this.dataHandling && this.houseRules)
+    return !(this.dataProtection && this.dataHandling && this.houseRules);
   }
 
   sendBooking() {
     this.showLoading = true;
-    this.reCaptchaV3Service.execute(HttpConstants.reCaptchaSiteKey, 'booking', (token) => {
-      const bookingData = new BookingData(this.booking.email,
-                                          this.booking.message,
-                                          this.booking.arrival,
-                                          this.booking.departure,
-                                          this.booking.fname,
-                                          this.booking.lname,
-                                          this.booking.phone,
-                                          this.booking.subscribe,
-                                          this.booking.personCount,
-                                          this.booking.petCount);
+    this.reCaptchaV3Service.execute(
+      HttpConstants.reCaptchaSiteKey,
+      "booking",
+      (token) => {
+        const bookingData = new BookingData(
+          this.booking.email,
+          this.booking.message,
+          this.booking.arrival,
+          this.booking.departure,
+          this.booking.fname,
+          this.booking.lname,
+          this.booking.phone,
+          this.booking.subscribe,
+          this.booking.personCount,
+          this.booking.petCount
+        );
 
-      this.calendarHttpService.submitBooking(bookingData, token).subscribe((bookingResult: any) => {
-        this.showLoading = false;
+        this.calendarHttpService
+          .submitBooking(bookingData, token)
+          .subscribe((bookingResult: any) => {
+            this.showLoading = false;
 
-        this.bookingStage = 'result';
-        this.bookingResult = 'failed';
+            this.bookingStage = "result";
+            this.bookingResult = "failed";
 
-        const data = bookingResult.response;
-        if (data.enabled === true && data.reservationCause === 'SUCCESS') {
-          this.bookingStage = 'result';
-          this.bookingResult = 'success';
+            const data = bookingResult.response;
+            if (data.enabled === true && data.reservationCause === "SUCCESS") {
+              this.bookingStage = "result";
+              this.bookingResult = "success";
 
-          this.booking.reservationId = data.reservationId;
-          this.scrollToPosition()
+              this.booking.reservationId = data.reservationId;
+              this.scrollToPosition();
 
-          this.googleAnalyticsService.eventEmitter(
-            GoogleAnalyticsConstants.SEND_BOOKING_SUBMIT_EVENT,
-            GoogleAnalyticsConstants.SUCCEED_ACTION,
-            '',
-            1)
-        } else {
-          this.googleAnalyticsService.eventEmitter(
-            GoogleAnalyticsConstants.SEND_BOOKING_SUBMIT_EVENT,
-            GoogleAnalyticsConstants.FAILED_ACTION,
-            '',
-            1)
-        }
-      });
-    }, {
-        useGlobalDomain: false
-    });
+              this.googleAnalyticsService.eventEmitter(
+                GoogleAnalyticsConstants.SEND_BOOKING_SUBMIT_EVENT,
+                GoogleAnalyticsConstants.SUCCEED_ACTION,
+                "",
+                1
+              );
+            } else {
+              this.googleAnalyticsService.eventEmitter(
+                GoogleAnalyticsConstants.SEND_BOOKING_SUBMIT_EVENT,
+                GoogleAnalyticsConstants.FAILED_ACTION,
+                "",
+                1
+              );
+            }
+          });
+      },
+      {
+        useGlobalDomain: false,
+      }
+    );
 
     if (this.booking.subscribe) {
-      const subscriptionModel = new SubscriptionModel(this.booking.email, this.booking.fname, this.booking.lname)
-      this.mailchimpService.submitSubscription(subscriptionModel).subscribe( result => {
-        console.log('success')
-      },
-      error => {
-        console.log(error)
-      },
-      () => {
-        console.log('else')
-      });
+      const subscriptionModel = new SubscriptionModel(
+        this.booking.email,
+        this.booking.fname,
+        this.booking.lname
+      );
+      this.mailchimpService.submitSubscription(subscriptionModel).subscribe(
+        (result) => {
+          console.log("success");
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          console.log("else");
+        }
+      );
 
       this.googleAnalyticsService.eventEmitter(
         GoogleAnalyticsConstants.SUBSCRIBE_TO_NEWSLETTER_CHECKBOX_EVENT,
         GoogleAnalyticsConstants.TRUE_ACTION,
-        '',
-        1)
+        "",
+        1
+      );
     } else {
       this.googleAnalyticsService.eventEmitter(
         GoogleAnalyticsConstants.SUBSCRIBE_TO_NEWSLETTER_CHECKBOX_EVENT,
         GoogleAnalyticsConstants.FALSE_ACTION,
-        '',
-        1)
+        "",
+        1
+      );
     }
 
     this.googleAnalyticsService.eventEmitter(
       GoogleAnalyticsConstants.SEND_BOOKING_SUBMIT_EVENT,
       GoogleAnalyticsConstants.OPEN_ACTION,
-      '',
-      1)
+      "",
+      1
+    );
   }
 
   onBack(stateName: number): void {
     switch (stateName) {
       case 0:
-        this.bookingStage = 'person'
+        this.bookingStage = "person";
         break;
       case 1:
-        this.bookingStage = 'data'
+        this.bookingStage = "data";
         break;
     }
 
-    this.scrollToPosition()
+    this.scrollToPosition();
   }
 
   closeCalendar(): void {
@@ -339,18 +381,18 @@ export class BookingComponent implements OnInit {
     if (amount > 1000) {
       returnData = [
         returnData.slice(0, thousandIndex),
-        ' ',
-        returnData.slice(thousandIndex)
-      ].join('');
+        " ",
+        returnData.slice(thousandIndex),
+      ].join("");
     }
 
     // 100 000 000, 10 000 000, 1 000 000
     if (amount > 1000000) {
       returnData = [
         returnData.slice(0, millionIndex),
-        ' ',
-        returnData.slice(millionIndex)
-      ].join('');
+        " ",
+        returnData.slice(millionIndex),
+      ].join("");
     }
 
     return returnData;
@@ -358,11 +400,11 @@ export class BookingComponent implements OnInit {
 
   ngOnInit() {
     this.formName = this.fb.group({
-      fname: ['', [Validators.required]],
-      lname: ['', [Validators.required]],
-      phone: ['', [Validators.required]],
-      email: ['', [Validators.required, CustomValidator.emailValidator]],
-      message: ['', []]
+      fname: ["", [Validators.required]],
+      lname: ["", [Validators.required]],
+      phone: ["", [Validators.required]],
+      email: ["", [Validators.required, CustomValidator.emailValidator]],
+      message: ["", []],
     });
     this.booking.arrival.setDate(this.booking.arrival.getDate() + 1);
     this.booking.departure.setDate(this.booking.departure.getDate() + 2);
@@ -373,42 +415,49 @@ export class BookingComponent implements OnInit {
         this.reservedDates = reservedDates.response.reservations;
       });
 
-    this.reCaptchaV3Service.execute(HttpConstants.reCaptchaSiteKey, 'booking', (token) => {
-    }, {
-        useGlobalDomain: false
-    });
+    this.reCaptchaV3Service.execute(
+      HttpConstants.reCaptchaSiteKey,
+      "booking",
+      (token) => {},
+      {
+        useGlobalDomain: false,
+      }
+    );
 
     this.showLoading = this.bookingEnabled;
-    this.costCalculationHttpService.getCostCalculationBetweenDate(this.booking.arrival, this.booking.departure).subscribe(
-      (costData: any) => {
+    this.costCalculationHttpService
+      .getCostCalculationBetweenDate(
+        this.booking.arrival,
+        this.booking.departure
+      )
+      .subscribe((costData: any) => {
         this.basePrice = costData.response.value;
         this.setPrice();
         this.showLoading = false;
-      }
-    );
+      });
   }
 
   getMonthNameById(id: number) {
     return Constants.months[id];
   }
 
-  scrollToPosition(x: number =0, y: number=0) {
+  scrollToPosition(x: number = 0, y: number = 0) {
     window.scrollTo(x, y);
   }
 
   handleReset() {
-    console.log('handleReset');
+    console.log("handleReset");
   }
 
   handleReady() {
-    console.log('handleReady');
+    console.log("handleReady");
   }
 
   handleLoad() {
-    console.log('handleLoad');
+    console.log("handleLoad");
   }
 
   handleSuccess(event) {
-    console.log('handleSuccess: ' + event);
+    console.log("handleSuccess: " + event);
   }
 }
